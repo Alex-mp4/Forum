8bb4f431e30e3b73611f8944858298a5a9429829
Alex-mp4 <alexander.heino@elev.ga.ntig.se>
2023-03-24 13:33:05 +0100

    init. project

--- src/MVC/view.java
+++ src/MVC/view.java
@@ -1,4 +1,49 @@
 package MVC;
 
+import javax.swing.*;
+
 public class view {
+    private JTextArea textArea1;
+    private JPanel panel1;
+    private JButton loginButton;
+    private JButton newPostButton;
+
+    public view(JTextArea textArea1, JPanel panel1, JButton loginButton, JButton newPostButton) {
+        this.textArea1 = textArea1;
+        this.panel1 = panel1;
+        this.loginButton = loginButton;
+        this.newPostButton = newPostButton;
+    }
+
+    public JTextArea getTextArea1() {
+        return textArea1;
+    }
+
+    public void setTextArea1(JTextArea textArea1) {
+        this.textArea1 = textArea1;
+    }
+
+    public JPanel getPanel1() {
+        return panel1;
+    }
+
+    public void setPanel1(JPanel panel1) {
+        this.panel1 = panel1;
+    }
+
+    public JButton getLoginButton() {
+        return loginButton;
+    }
+
+    public void setLoginButton(JButton loginButton) {
+        this.loginButton = loginButton;
+    }
+
+    public JButton getNewPostButton() {
+        return newPostButton;
+    }
+
+    public void setNewPostButton(JButton newPostButton) {
+        this.newPostButton = newPostButton;
+    }
 }
---
+++ src/README.md
@@ -0,0 +1,11 @@
+## Forum | prg2 TE20
+
+Forumet ska kunna ta information från databasen och göra en snarlik (typ) version jämfört med den gjord under webbserverprogrammeringen.
+
+#### 2023-03-24:
+1. Skapade filer (MVC)
+2. Kopplade mysql
+3. Gjorde en vacker view
+4. Genererade kod i view
+
+Jag tror att jag kan lista ut vad som pågår, men vi lär ju märka.
